185b2f3cd5c6ea778de623feba46d37d215c0411
WillLewis <willxemail@gmail.com>
2023-05-12 00:18:07 -0500

    update to gpt-3.5, added role and content message for context, add ability to call with one line of context or multiple

---
+++ ai.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Temporary file to store command and response history
+HISTORY_FILE="/tmp/command_history.txt"
+
+# Extract the optional number of history entries argument
+history_entries="$1"
+if [[ "$history_entries" =~ ^-?[0-9]+$ ]]; then
+  # If the argument is a number, shift the arguments to remove it from the list
+  shift
+else
+  # If no number is provided, use a default value
+  history_entries=1
+fi
+
+# Check if the command is "chat"
+if [[ "$1" == "chat" ]]; then
+  node index.js "$history_entries"
+else
+  # Check for the #chat# keyword
+  for arg in "$@"; do
+    if [[ "$arg" == "#chat#" ]]; then
+      chat=true
+      break
+    fi
+  done
+
+  # Check if the command includes '|chat|' or '|'
+  if [[ $* == *"|"* ]]; then
+    # Split the command into the part before '|chat|' or '|' and the part after
+    IFS='|' read -ra ADDR <<< "$*"
+    COMMAND="${ADDR[0]}"
+    CHAT="${ADDR[1]}"
+
+    # If '|chat|' is present, use the part after '|chat|' as the chat input
+    if [[ $CHAT == "chat" ]]; then
+      CHAT="${ADDR[2]}"
+    fi
+
+    # Run the command and capture the output
+    OUTPUT=$(eval $COMMAND)
+    echo -e "Command: $COMMAND\nResponse:\n$OUTPUT\n----------" >> /tmp/command_history.txt
+    node ./index.js "$CHAT"
+  else
+    # Execute the command and capture both stdout and stderr
+    output=$(eval "$@" 2>&1)
+
+    # Save the command and output as variables
+    command="$@"
+    response="$output"
+
+    # Append the command and response to the history file
+    echo "Command: $command" >> "$HISTORY_FILE"
+    echo "Response:" >> "$HISTORY_FILE"
+    echo "$response" >> "$HISTORY_FILE"
+    echo "----------" >> "$HISTORY_FILE"
+  fi
+fi
--- index.js
+++ index.js
@@ -8,44 +8,94 @@ const openai = new OpenAIApi(configuration);
 const fs = require('fs');
 const HISTORY_FILE = '/tmp/command_history.txt';
 
-function readLastNLines(file, numLines) {
-  const data = fs.readFileSync(file, 'utf8');
-  const lines = data.trim().split('\n');
-  return lines.slice(Math.max(lines.length - numLines, 0)).join('\n');
+
+// Read command/response pairs from a file
+function readLastNPairs(filePath, numPairs) {
+  const lines = fs.readFileSync(filePath, 'utf8').split('\n');
+  let pairs = [];
+  let command = '';
+  let response = [];
+  let currentRole = '';
+
+  for (let line of lines) {
+      if (line.startsWith('Command: ')) {
+          if (currentRole === 'response') {  // Save previous command/response pair
+              pairs.push({ command: command, response: response.join('\n') });
+              response = [];  // Reset response
+          }
+          command = line.replace('Command: ', '');
+          currentRole = 'command';
+      } else if (line.startsWith('Response:')) {
+          currentRole = 'response';
+      } else if (line === '----------') {
+          if (currentRole === 'response') {  // Save previous command/response pair
+              pairs.push({ command: command, response: response.join('\n') });
+              response = [];  // Reset response
+          }
+          currentRole = '';
+      } else if (currentRole === 'response') {
+          response.push(line);
+      }
+  }
+  // Save the last command/response pair if any
+  if (command && response.length) {
+      pairs.push({ command: command, response: response.join('\n') });
+  }
+
+  // Only keep the last numPairs pairs
+  return pairs.slice(-numPairs);
 }
 
 
-// Function to send a request to ChatGPT API
-async function chatGPTRequest(prompt) {
-    try {
-        const response = await openai.createCompletion({
-          model: 'text-davinci-003',
-          prompt: prompt,
-          max_tokens: 150,
-          top_p: 0.7,
-          stop: null, 
-          temperature: 0.5,
-        });
-      console.log(response);  
-      console.log(response.data.choices[0].text);
-    } catch (error) {
-      console.error('Error:', error);
-    }
+
+async function chatGPTRequest(messages) {
+  try {
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+      max_tokens: 150,
+      top_p: 0.7,
+      stop: null, 
+      temperature: 0.5,
+    });
+    console.log(response);
+    console.log(response.data.choices[0].message.content);
+  } catch (error) {
+    console.error('Error:', error);
   }
+}
+
   
-  // Create readline interface
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+// Get the number of command/response pairs from the arguments
+const numPairs = process.argv[2] || 1;
+
   
-  rl.question('Type your question for ChatGPT: ', (input) => {
-    if (input.trim()) {
-      const lastNLines = readLastNLines(HISTORY_FILE, 20); // Adjust the number of lines to capture the desired number of commands and responses
-      const prompt = `Last commands and their responses:\n${lastNLines}\n${input}`;
-      chatGPTRequest(prompt);
-    } else {
-      console.log('Invalid input. Please type a question for ChatGPT.');
-    }
-    rl.close();
-  });
+// Create readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+  
+// Prompt formation
+rl.question('Type your question for ChatGPT: ', (input) => {
+  if (input.trim()) {
+    const lastNPairs = readLastNPairs(HISTORY_FILE, numPairs);
+    
+    let messages = [{role: 'system', content: 'You are a helpful assistant that provides information about terminal commands and how to debug terminal errors. The user will provide a command and the response it yielded, then ask a question about it.'}];
+    // Add each command-response pair as its own user-assistant message pair
+    lastNPairs.forEach(pair => {
+      messages.push({role: 'user', content: `Command: ${pair.command}`});
+      messages.push({role: 'assistant', content: `Response: ${pair.response}`});
+    });
+    // Add the user's new input
+    messages.push({role: 'user', content: input});
+    chatGPTRequest(messages);
+  } else {
+    console.log('Invalid input. Please type a question for ChatGPT.');
+  }
+  rl.close();
+});
+
+
+
